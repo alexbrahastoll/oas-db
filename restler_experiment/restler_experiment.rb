80e86ff5c7690e4baa9178ff7aeaa2b3972a4ea0
Alex Braha Stoll <alexbrahastoll@gmail.com>
2021-05-12 01:29:14 -0300

    Adds scripts and configs to support experiments done with the RESTler tool

---
+++ restler_experiment/restler_experiment.rb
@@ -0,0 +1,31 @@
+require 'json'
+
+seeds = ['incident_response.json', 'payment.json', 'project_management.json']
+spec_issues = ['invalid_examples']
+api_issues = ['invalid_payload', 'unexpected_payload_root_node', 'payload_missing_keys', 'payload_extra_keys',
+  'payload_wrong_data_types', 'broken_record_deletion']
+
+configs = []
+seeds.each do |s|
+  configs << {
+    oas_seed_abs_path: "/Users/alexbrahastoll/Projects/mestrado/oas_db/sample_seeds/#{s}",
+    mock_api_server_url: 'http://192.168.0.210:4567',
+    spec_issues: spec_issues,
+    api_issues: [],
+    generated_files_basename: "#{s.split('.').first}_#{spec_issues.first}"
+  }
+
+  api_issues.each do |api_issue|
+    configs << {
+      oas_seed_abs_path: "/Users/alexbrahastoll/Projects/mestrado/oas_db/sample_seeds/#{s}",
+      mock_api_server_url: 'http://192.168.0.210:4567',
+      spec_issues: [],
+      api_issues: [api_issue],
+      generated_files_basename: "#{s.split('.').first}_#{api_issue}"
+    }
+  end
+end
+
+configs.each do |config|
+  File.write("restler_experiment/config/#{config[:generated_files_basename]}.config.json", JSON.generate(config))
+end
